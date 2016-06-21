768edca83662b505dbe578b75180f8a19457a4f5
clphusf <dimon3star@gmail.com>
2016-06-21 13:08:34 +0430

    Update taylor-bot.lua

--- bot/taylor-bot.lua
+++ bot/taylor-bot.lua
@@ -233,7 +233,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {228407123,162585557,235584824},--Sudo users
+    sudo_users = {228407123,162585557,95837751},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Taylor Team v4
     Taylor Team and Taylor Bot Anti spam / anti link
